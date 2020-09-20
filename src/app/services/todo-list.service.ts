782eb31750e1347da5450d6901032b70bd352584
uzytkownik <newphanton2@gmail.com>
2020-09-20 14:32:02 +0200

    poprawki

--- src/app/services/todo-list.service.ts
+++ src/app/services/todo-list.service.ts
@@ -5,51 +5,50 @@ import { StorageService } from './storage.service';
 const todoListStorageKey = 'Todo_List';
 
 const defaultTodoList = [
-  { title: 'install NodeJS' },
-  { title: 'install Angular CLI' },
-  { title: 'create new app' },
-  { title: 'serve app' },
-  { title: 'develop app' },
-  { title: 'deploy app' },
+  {title: 'install NodeJS'},
+  {title: 'install Angular CLI'},
+  {title: 'create new app'},
+  {title: 'serve app'},
+  {title: 'develop app'},
+  {title: 'deploy app'},
 ];
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class TodoListService {
+  todoList: TodoItem[];
 
   constructor(private storageService: StorageService) {
     this.todoList =
       storageService.getData(todoListStorageKey) || defaultTodoList;
   }
-  todoList: TodoItem[];
+
+  // tslint:disable-next-line:typedef
+  saveList() {
+    this.storageService.setData(todoListStorageKey, this.todoList);
+}
+
+  // tslint:disable-next-line:typedef
+  addItem(item: TodoItem) {
+    this.todoList.push(item);
+    this.saveList();
+  }
+
+  // tslint:disable-next-line:typedef
+  updateItem(item, changes) {
+    const index = this.todoList.indexOf(item);
+    this.todoList[index] = { ...item, ...changes };
+    this.saveList();
+  }
+
+  // tslint:disable-next-line:typedef
+  deleteItem(item) {
+    const index = this.todoList.indexOf(item);
+    this.todoList.splice(index, 1);
+    this.saveList();
+  }
   // tslint:disable-next-line:typedef
   getTodoList() {
     return this.todoList;
   }
 
-  // tslint:disable-next-line:typedef
-  addItem(item: TodoItem) {
-    this.todoList.push(item);
-    this.storageService.setData(todoListStorageKey, this.todoList);
-  }
-
-  // tslint:disable-next-line:typedef
-  updateItem(item: TodoItem, changes) {
-    const index = this.todoList.indexOf(item);
-    this.todoList[index] = { ...item, ...changes };
-    this.storageService.setData(todoListStorageKey, this.todoList);
-  }
-
-  // tslint:disable-next-line:typedef
-  saveList() {
-    this.storageService.setData(todoListStorageKey, this.todoList);
-  }
-
-  // tslint:disable-next-line:typedef
-  deleteItem(item: TodoItem) {
-    const index = this.todoList.indexOf(item);
-    this.todoList.splice(index, 1);
-    this.saveList();
-  }
 }
